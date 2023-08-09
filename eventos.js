6584cb81c132cb318db41ae016d6b760a11d4bb5
diegoalvite <dalvite91@gmail.com>
2023-08-08 23:16:32 -0300

    java

---
+++ eventos.js
@@ -0,0 +1,7 @@
+// elemento por su clase "div"
+const divElement = document.querySelector("#conteiner");
+//se agrega el manejador del evento para que al hacer click me salte la alerta 'Hola! Soy el div'
+divElement.addEventListener("click", () => {
+  alert("Hola! Soy el div");
+});
+console.log;
